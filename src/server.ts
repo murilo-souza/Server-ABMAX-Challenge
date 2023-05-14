885d9468c4b4a6ed9c71f511451a2bb05ae00a20
Murilo Leme de Souza <murilo.l.souza232@gmail.com>
2023-05-14 20:19:18 -0300

    using star wars API to backend data

--- src/server.ts
+++ src/server.ts
@@ -7,14 +7,35 @@ const app = express()
 app.use(express.json())
 app.use(cors());
 
-app.get('/api/rota', async (req, res) => {
+app.get('/characters', async (req, res) => {
   try {
-    const response = await axios.get('https://swapi.dev/api/people/');
-    console.log(response)
+    const response = await axios.get('https://swapi.dev/api/people');
     res.json(response.data);
   } catch (error) {
     console.error(error);
     res.status(500).send('Erro ao chamar a API');
   }
 });
+
+app.get('/planets', async (req, res) => {
+  try {
+    const response = await axios.get('https://swapi.dev/api/planets');
+    res.json(response.data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Erro ao chamar a API');
+  }
+});
+
+app.get('/starships', async (req, res) => {
+  try {
+    const response = await axios.get('https://swapi.dev/api/starships');
+    res.json(response.data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Erro ao chamar a API');
+  }
+});
+
+app.listen(3333)
 
